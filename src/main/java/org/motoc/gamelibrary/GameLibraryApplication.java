44528601c00058f9e9a6722206da2ffef07a31b1
xxjokerx <rouzic.jeremie@gmail.com>
2020-05-01 14:32:06 +0200

    initial commit, springboot project done, gitignore, MIT LICENCE, 0.0.1-SNAPSHOT

---
+++ src/main/java/org/motoc/gamelibrary/GameLibraryApplication.java
@@ -0,0 +1,13 @@
+package org.motoc.gamelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/motoc/gamelibrary/GameLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.motoc.gamelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
