f70fa43834a99f6b31c4031e5231f45658669337
xxjokerx <rouzic.jeremie@gmail.com>
2020-05-21 15:42:40 +0200

    added ThemeDto

---
+++ src/main/java/org/motoc/gamelibrary/dto/ThemeDto.java
@@ -0,0 +1,15 @@
+package org.motoc.gamelibrary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ThemeDto {
+
+    private long id;
+    private String name;
+
+}
