54f6b047636fa8eef7c0e4cd7d3237eab289ab7b
xxjokerx <rouzic.jeremie@gmail.com>
2021-02-25 15:10:18 +0100

    remove comments, add remove / add game's expansion in repo

--- src/main/java/org/motoc/gamelibrary/repository/criteria/GameRepositoryCustom.java
+++ src/main/java/org/motoc/gamelibrary/repository/criteria/GameRepositoryCustom.java
@@ -1,7 +1,9 @@
 package org.motoc.gamelibrary.repository.criteria;
 
 import org.motoc.gamelibrary.dto.GameNameDto;
+import org.motoc.gamelibrary.model.Game;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -15,4 +17,12 @@ public interface GameRepositoryCustom {
      * Get all Games' name in a custom DTO
      */
     List<GameNameDto> findNames();
+
+    Game addExpansions(Game game, @NotNull List<Game> expansions);
+
+    Game addCoreGame(Game game, @NotNull Game coreGame);
+
+    void removeCoreGame(Game game);
+
+    void removeExpansion(Game game, Game expansion);
 }
--- src/main/java/org/motoc/gamelibrary/repository/criteria/implementation/GameRepositoryCustomImpl.java
+++ src/main/java/org/motoc/gamelibrary/repository/criteria/implementation/GameRepositoryCustomImpl.java
@@ -1,7 +1,10 @@
 package org.motoc.gamelibrary.repository.criteria.implementation;
 
 import org.motoc.gamelibrary.dto.GameNameDto;
+import org.motoc.gamelibrary.model.Game;
 import org.motoc.gamelibrary.repository.criteria.GameRepositoryCustom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -16,6 +19,8 @@ import java.util.List;
  */
 @Repository
 public class GameRepositoryCustomImpl implements GameRepositoryCustom {
+
+    private static final Logger logger = LoggerFactory.getLogger(GameRepositoryCustomImpl.class);
 
     private final EntityManager entityManager;
 
@@ -33,4 +38,50 @@ public class GameRepositoryCustomImpl implements GameRepositoryCustom {
         return q.getResultList();
     }
 
+    @Override
+    public Game addExpansions(Game game, List<Game> expansions) {
+        for (Game expansion : expansions) {
+            if (!game.getExpansions().contains(expansion))
+                game.addExpansion(expansion);
+            else
+                logger.warn("Expansion {} of id={} is already linked to {} of id={}",
+                        expansion.getName(), expansion.getId(),
+                        game.getName(), game.getId());
+        }
+        entityManager.persist(game);
+        logger.info("Successfully persisted game of id={}", game.getId());
+        return game;
+    }
+
+    @Override
+    public Game addCoreGame(Game game, Game coreGame) {
+        Game coreGameFromDB = entityManager.find(Game.class, coreGame.getId());
+        if (coreGameFromDB != null) {
+            game.addCoreGame(coreGameFromDB);
+            logger.info("Successfully persisted category of id={}", game.getId());
+        } else {
+            logger.warn("No core game of id {}", coreGame.getId());
+        }
+        return game;
+    }
+
+    @Override
+    public void removeCoreGame(Game game) {
+        game.removeCoreGame();
+        entityManager.persist(game);
+        logger.info("Successfully removed core game of game of id={}", game.getId());
+    }
+
+    @Override
+    public void removeExpansion(Game game, Game expansion) {
+        Game gameFromDB = entityManager.find(Game.class, game.getId());
+        Game expansionFromDB = entityManager.find(Game.class, expansion.getId());
+
+        if (gameFromDB.getExpansions().isEmpty() || !gameFromDB.getExpansions().contains(expansionFromDB))
+            logger.warn("Category of id=" + game.getId() + " does not contains this children of id=" + expansion.getId());
+        else {
+            gameFromDB.removeExpansion(expansionFromDB);
+            entityManager.persist(gameFromDB);
+        }
+    }
 }
