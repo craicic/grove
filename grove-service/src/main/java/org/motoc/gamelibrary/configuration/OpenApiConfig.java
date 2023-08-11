84a5d2383ff1239fbbfcdc2134aa427b4c9289ce
xxjokerx <common.hori@protonmail.com>
2023-08-11 18:25:15 +0200

    Improved OpenAPI configuration

--- grove-service/src/main/java/org/motoc/gamelibrary/configuration/OpenApiConfig.java
+++ grove-service/src/main/java/org/motoc/gamelibrary/configuration/OpenApiConfig.java
@@ -2,33 +2,24 @@ package org.motoc.gamelibrary.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.motoc.gamelibrary.technical.properties.OpenApiProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition
-@io.swagger.v3.oas.annotations.security.SecurityScheme(type = SecuritySchemeType.HTTP,
+@io.swagger.v3.oas.annotations.security.SecurityScheme(
+        type = SecuritySchemeType.HTTP,
         scheme = "basic",
         name = "basicAuth")
 public class OpenApiConfig {
-    private static final String SCHEME_NAME = "basicAuth";
-    private static final String SCHEME = "basic";
 
     @Bean
     public OpenAPI customOpenAPI(OpenApiProperties properties) {
-        return new OpenAPI()
-                .info(getInfo(properties))
-//                .components(new Components()
-//                        .addSecuritySchemes(SCHEME_NAME, createSecurityScheme()))
-//                .addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME))
-                ;
+        return new OpenAPI().info(getInfo(properties));
     }
 
     private Info getInfo(OpenApiProperties properties) {
@@ -43,12 +34,5 @@ public class OpenApiConfig {
         return new License()
                 .name("Unlicense")
                 .url("https://unlicense.org/");
-    }
-
-    private SecurityScheme createSecurityScheme() {
-        return new SecurityScheme()
-                .name(SCHEME_NAME)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme(SCHEME);
     }
 }
