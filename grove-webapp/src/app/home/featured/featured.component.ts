ba5235e2071f28b8cee47f7041ea347b15f8cb82
xxjokerx <common.hori@protonmail.com>
2023-10-18 08:17:58 +0200

    work to include the game featured page

---
+++ grove-webapp/src/app/home/featured/featured.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured',
+  templateUrl: './featured.component.html',
+  styleUrls: ['./featured.component.css']
+})
+export class FeaturedComponent {
+
+}
